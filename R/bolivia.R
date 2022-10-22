abd1083e661a7e75758774c3a46434cb069a5505
mohemeng <116262711+mohemeng@users.noreply.github.com>
2022-10-21 21:09:33 -0700

    Add files via upload

---
+++ R/bolivia.R
@@ -0,0 +1,27 @@
+#' finds the mean of an array
+#'
+#' @param x a numerical input
+#'
+#' @return the final value
+#' @export
+#'
+#' @examples last(runif(10))
+last <- function(x) {
+
+  mean(x)
+}
+
+
+#' Finds the sum of two values
+#'
+#' @param a first value
+#' @param b second value
+#'
+#' @return a sum of the first and second values
+#' @export
+#'
+#' @examples jack(2, 3)
+jack <- function(a, b){
+
+  a + b
+}
---
+++ R/nobody.R
@@ -0,0 +1,11 @@
+#' Brings forth the product
+#'
+#' @param x an array of numbers
+#'
+#' @return a product of all the members
+#' @export
+#'
+#' @examples prod(runif(10))
+aden <- function(x){
+  prod(x)
+}
